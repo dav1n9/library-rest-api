655c26d35156fca74e6902b8415c1002484439c6
Davin <85967571+dav1n9@users.noreply.github.com>
2024-07-06 11:34:27 +0900

    refactor: 회원의 대출 가능 상태 검증 에러 메시지 구체화
    - 패널티 여부와 연체 도서 여부에 따른 에러 메시지를 구분하여 작성

--- src/main/java/com/example/libraryrestapi/constants/ErrorMessage.java
+++ src/main/java/com/example/libraryrestapi/constants/ErrorMessage.java
@@ -3,7 +3,7 @@ package com.example.libraryrestapi.constants;
 public class ErrorMessage {
     public static final String NOT_FOUND_USER = "존재하지 않는 회원입니다.";
     public static final String NOT_FOUND_BOOK = "존재하지 않는 도서입니다.";
-    public static final String USER_NOT_AVAILABLE_FOR_RENT = "대출이 불가능한 회원입니다.";
+    public static final String USER_NOT_AVAILABLE_FOR_RENT = "해당 회원은 패널티 기간 중이므로 대출이 불가능합니다.";
     public static final String BOOK_NOT_RENTED = "대출되지 않은 도서입니다.";
     public static final String BOOK_ALREADY_RENTED = "이미 대출된 도서입니다.";
     public static final String USER_HAS_UNRETURNED_BOOKS = "반납하지 않은 도서가 있어 대출이 불가능합니다.";
--- src/main/java/com/example/libraryrestapi/service/BookService.java
+++ src/main/java/com/example/libraryrestapi/service/BookService.java
@@ -111,31 +111,24 @@ public class BookService {
 
     /**
      * 대출을 할 수 있는 회원인지 확인하는 메소드.
+     * 패널티 기간이 끝났는지 확인하고, 연체 도서가 있는지 확인하여
+     * 회원의 대출 가능 여부를 판단한다.
      * @param user 확인할 회원
      * @throws IllegalArgumentException 대출이 불가능한 회원인 경우 발생하는 예외
      */
     private void validateUserForRent(User user) {
-        if (!isRentAllowed(user)) {
-            throw new IllegalArgumentException(ErrorMessage.USER_NOT_AVAILABLE_FOR_RENT);
-        }
-    }
-
-    /**
-     * 해당 회원이 대출이 가능한 상태인지 확인하는 메소드.
-     * 패널티 기간이 끝났는지 확인하고, 연체 도서가 있는지 확인하여
-     * 회원의 대출 가능 여부를 판단한다.
-     * @return 회원이 대출 가능하면 true, 불가능하면 false.
-     */
-    private boolean isRentAllowed(User user) {
+        // 1. 패널티 기간이 끝났는지 확인
         LocalDateTime current = LocalDateTime.now();
-        // 패널티 기간이 끝났는지 확인
         boolean isPenaltyPeriodOver = user.getPenaltyEndDate() == null || current.isAfter(user.getPenaltyEndDate());
-        // 연체 도서가 있는지 확인
+        if (isPenaltyPeriodOver)
+            throw new IllegalArgumentException(ErrorMessage.USER_NOT_AVAILABLE_FOR_RENT);
+
+        // 2. 연체 도서가 있는지 확인
         boolean hasOverdueBooks = user.getRents().stream()
                 .anyMatch(rent -> rent.getReturnStatus() == RentStatus.RENTED
                         && rent.getRentDate().plusDays(7).isBefore(current));
-
-        return isPenaltyPeriodOver && !hasOverdueBooks;
+        if (hasOverdueBooks)
+            throw new IllegalArgumentException(ErrorMessage.USER_HAS_UNRETURNED_BOOKS);
     }
 
     /**
